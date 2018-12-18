bc1b4cfcd585c381ac4c47b47a995d8a965ccd71
chenfu2017 <21114056@qq.com>
2018-12-18 16:37:36 +0800

    Algorithm

--- leetcode/31.NextPermutation.py
+++ leetcode/31.NextPermutation.py
@@ -7,14 +7,16 @@ class Solution:
         i = len(nums) - 2
         while i >= 0 and nums[i + 1] <= nums[i]:
             i -= 1
+        print(i)
         if i >= 0:
             j = len(nums) - 1
             while j >= 0 and nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
+            print(j)
         nums[i + 1:] = sorted(nums[i + 1:])
         print(nums)
 
 
 # [4,2,0,3,0,2,2]
-Solution().nextPermutation([1, 2, 3])
+Solution().nextPermutation([1, 3, 2, 3])
