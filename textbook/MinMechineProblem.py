5a45af59eb4513d75d36710eee037fdc2040c7ee
chenfu2017 <21114056@qq.com>
2018-12-26 21:46:01 +0800

    leetcode

--- textbook/MinMechineProblem.py
+++ textbook/MinMechineProblem.py
@@ -21,11 +21,8 @@ def dfs(p, w, index, l):
             min_price = p
             r = l.copy()
         return
-    for i in range(len(price[0])):
-        l.append(i)
-        dfs(p + price[index][i], w + weight[index][i], index + 1, l)
-        l.pop()
-l = []
-dfs(0, 0, 0, l)
+    for i in range(len(price[index])):
+        dfs(p + price[index][i], w + weight[index][i], index + 1, l+[i])
+dfs(0, 0, 0, [])
 print("最小总价值为%d,供应商序号为:%s"%(min_price,r))
 
