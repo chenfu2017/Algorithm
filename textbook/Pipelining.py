e3210165abd2fb719a1a74819069d3d9b4223a2d
chenfu2017 <21114056@qq.com>
2019-01-07 20:05:45 +0800

    leetcode

--- textbook/Pipelining.py
+++ textbook/Pipelining.py
@@ -3,31 +3,27 @@ class Solution:
         self.bestf = float('inf')
         self.bestx = []
 
-    def dowork(self,f,s,i,f1,f2,x):
+    def dowork(self, f, s, i, f1, f2, x):
         n = len(f)
         if i == n:
-            if f2[i-1]<self.bestf:
-                self.bestf = f2[i-1]
-                self.bestx=x.copy()
-            print(f2)
+            if f2 < self.bestf:
+                self.bestf = f2
+                self.bestx = x.copy()
             return
         for j in range(i, n):
-            f1 +=f[x[j]]
-            f2[i]= max(f[x[j]], f2[i - 1]) + s[x[i]]
-            # if f2[i]<self.bestf:
-            x[i], x[j]= x[j], x[i]
-            self.dowork(f,s,i + 1, f1+f[x[j]],max(f1, f2[i - 1]) + s[x[i]],x)
-            x[i], x[j]= x[j], x[i]
-            f1-=f[x[j]]
+            f1 += f[x[j]]
+            if max(f2, f1) + s[x[i]] < self.bestf:
+                x[i], x[j] = x[j], x[i]
+                self.dowork(f, s, i + 1, f1, max(f2, f1) + s[x[i]], x)
+                x[i], x[j] = x[j], x[i]
+            f1 -= f[x[j]]
 
 
-f=[2,2,3]
-s=[3,1,1]
+f = [5, 12, 4, 8]
+s = [6, 2, 14, 7]
 n = len(f)
-f2 =[0]*n
-x =[i for i in range(n)]
+x = [i for i in range(n)]
 solution = Solution()
-solution.dowork(f,s,0,0,f2,x)
-
-
-
+solution.dowork(f, s, 0, 0, 0, x)
+print(solution.bestx)
+print(solution.bestf)
