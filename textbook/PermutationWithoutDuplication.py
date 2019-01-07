3ae44996138d01a6fe0fe3ae2d5b00e789fc8dd9
chenfu2017 <21114056@qq.com>
2019-01-07 14:12:22 +0800

    leetcode

--- textbook/PermutationWithoutDuplication.py
+++ textbook/PermutationWithoutDuplication.py
@@ -1,30 +1,39 @@
 class Solution:
-    list = []
-    visit = []
+    def __init__(self):
+        self.visit = []
+        self.res = []
 
-    def permute(self, nums):
-        self.list.clear()
-        if len(nums) == 0:
-            return list
-        l = []
-        self.visit = [False] * len(nums)
-        self.fun(nums, 0, l)
-        return self.list
+    def permute_1(self, list):
+        n = len(list)
+        self.visit = [False] * n
 
-    def fun(self, nums, index, l):
-        if index == len(nums):
-            if l not in self.list:
-                self.list.append(l.copy())
+        def permute_1_detail(index, l):
+            if index == n:
+                self.res.append(l)
+                return
+            for i in range(n):
+                if not self.visit[i]:
+                    self.visit[i] = True
+                    permute_1_detail(index + 1, l + [list[i]])
+                    self.visit[i] = False
+
+        permute_1_detail(0, [])
+
+    def permute_2(self, index, l):
+        n = len(l)
+        if index == n:
+            self.res.append(l.copy())
             return
-        for i in range(len(nums)):
-            if not self.visit[i]:
-                l.append(nums[i])
-                self.visit[i] = True
-                self.fun(nums, index + 1, l)
-                l.pop()
-                self.visit[i] = False
+        for i in range(index, n):
+            l[index], l[i] = l[i], l[index]
+            self.permute_2(index + 1, l)
+            l[index], l[i] = l[i], l[index]
 
 
-result = Solution().permute(['a', 'a', 'c', 'c'])
-for l in result:
-    print(l)
+s = Solution()
+l = ['a', 'b', 'c', 'd']
+# s.permute_1(l)
+s.permute_2(0, l)
+for r in s.res:
+    print(r)
+print(len(s.res))
--- textbook/TravellingSalesmanProblem.py
+++ textbook/TravellingSalesmanProblem.py
@@ -10,8 +10,9 @@ class Solution:
         self.visited = [False] * len(self.G)
         self.res = INF
         self.x = []
+        self.count = 0
 
-    def travel(self,index, c, cx):
+    def travel_1(self, index, c, cx):
         n = len(self.G)
         if index == n - 1:
             c += self.G[cx[-1]][cx[0]]
@@ -20,13 +21,32 @@ class Solution:
                 self.res = c
             return
         for i in range(n):
-            if not self.visited[i]:
+            self.count += 1
+            if not self.visited[i] and c + self.G[cx[-1]][i] < self.res:
                 self.visited[i] = True
-                self.travel( index + 1, c + self.G[cx[-1]][i], cx + [i])
+                self.travel_1(index + 1, c + self.G[cx[-1]][i], cx + [i])
                 self.visited[i] = False
+
+    def travel_2(self, s, c, cx):
+        n = len(self.G)
+        if s == n:
+            c += self.G[cx[-1]][cx[0]]
+            if c < self.res:
+                self.res = c
+                self.x = cx.copy()
+            return
+        for i in range(s, n):
+            self.count += 1
+            if self.G[cx[i - 1]][cx[i]] != 0 and c + self.G[cx[i - 1]][cx[i]] < self.res:
+                cx[i], cx[s] = cx[s], cx[i]
+                self.travel_2(s + 1, c + self.G[cx[s - 1]][cx[s]], cx)
+                cx[i], cx[s] = cx[s], cx[i]
 
 
 s = Solution()
-s.visited[0] = True
-s.travel(0, 0, [0])
+start = 3
+s.visited[start] = True
+# s.travel_1(0, 0, [start])
+s.travel_2(1, 0, [0, 1, 2, 3])
 print(s.res)
+print(s.count)
