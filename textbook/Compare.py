b425427cb6bcfa3eb4bd7587a099f350fc11adec
chenfu2017 <21114056@qq.com>
2019-01-09 13:32:19 +0800

    leetcode

--- textbook/Compare.py
+++ textbook/Compare.py
@@ -11,16 +11,14 @@ def fun(list):
     min = 0
     max = 0
     for i in range(n - 1):
-        m = heapq.nsmallest(2, l_min)
-        min += m[0] + m[1] - 1
-        heapq.heappop(l_min)
-        heapq.heappop(l_min)
-        heapq.heappush(l_min, m[0] + m[1])
-        n = heapq.nsmallest(2, l_max)
-        max += -n[0] - n[1] - 1
-        heapq.heappop(l_max)
-        heapq.heappop(l_max)
-        heapq.heappush(l_max, n[0] + n[1])
+        m1 = heapq.heappop(l_min)
+        m2 = heapq.heappop(l_min)
+        min += m1 + m2 - 1
+        heapq.heappush(l_min, m1 + m2)
+        n1 = heapq.heappop(l_max)
+        n2 = heapq.heappop(l_max)
+        max += -n1 - n2 - 1
+        heapq.heappush(l_max, n1 + n2)
     print("最少比较次数为%d，最多比较次数为%d" % (min, max))
 
 
